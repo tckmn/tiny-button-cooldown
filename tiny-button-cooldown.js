cb391a44c5adeca934686aa5dfb3c8e5bcf07e66
KeyboardFire <andy@keyboardfire.com>
2014-06-25 13:02:41 -0500

    some bugfixes, more customizability and examples, extensibility

--- tiny-button-cooldown.js
+++ tiny-button-cooldown.js
@@ -1,10 +1,13 @@
 function Cooldown(options) {
+	if (!options) options = {};
+	
 	if (!options.padding) options.padding = 0;
-	options.buttons = $(options.buttons); // verify just in case
+	if (!options.buttons) options.buttons = $();
+	options.buttons = $(options.buttons); // verify that buttons is a jQuery element
 	
 	for (x in options) {
-		// call functions specified in constructor, if possible (ex. autoSetup: true)
-		if (options[x] === true && typeof this[x] === 'function') this[x]();
+		// call functions specified in constructor, if possible (ex. autoSetupAll: true)
+		if (typeof this[x] === 'function') this[x](options[x]);
 		else {
 			// proceed normally
 			this[x] = options[x];
@@ -15,9 +18,9 @@ function Cooldown(options) {
 }
 Cooldown.prototype.initialize = function() {
 	this.setupButtons();
-	if (this.toAutoSetupInInitialize) this._autoSetup();
+	if (this.toAutoSetupAllInInitialize) this._autoSetupAll();
 	
-	return this; // chain
+	return this;
 };
 Cooldown.prototype.setupButtons = function() {
 	this.buttons.css({
@@ -27,49 +30,59 @@ Cooldown.prototype.setupButtons = function() {
 		.text('\u00A0')
 		.addClass('_tiny_button_cooldown_progress-bar')
 		.css({
+			fontSize: '1px',
 			position: 'absolute',
 			top: '0px',
 			left: '0px',
-			backgroundColor: '#000', // TODO make customizable
-			opacity: '0.5',
 			width: '0px',
 			height: '0px'
 		}));
 		
-	return this; // chain
+	return this;
 };
-Cooldown.prototype.autoSetup = function() {
-	this.toAutoSetupInInitialize = true;
+Cooldown.prototype.autoSetup = function($el, options) {
+	var cooldown = this;
+	$($el).click(function() {
+		cooldown.cooldown($el, options);
+	});
 	
-	return this; // chain
+	return this;
 };
-Cooldown.prototype._autoSetup = function() {
+Cooldown.prototype.autoSetupAll = function(toAutoSetupAllInInitialize) {
+	this.toAutoSetupAllInInitialize = toAutoSetupAllInInitialize;
+	
+	return this;
+};
+Cooldown.prototype._autoSetupAll = function() {
 	var cooldown = this;
 	this.buttons.each(function() {
-		// verify jQuery object first, just in case
-		$(this).click(function() {
-			cooldown.cooldown(this);
-		});
+		cooldown.autoSetup(this);
 	});
 };
-Cooldown.prototype.cooldown = function($el, callback, length) {
-	// validate element to cool down, make sure it's a jQuery object
+Cooldown.prototype.cooldown = function($el, options) {
+	// basic validation
+	if (!options) options = {};
 	$el = $($el);
-	
 	// is there already a cooldown happening?
 	if ($el.is('[disabled]')) return;
 	
 	// look everywhere we can to find a cooldown amount, and error out if there is none
-	var cooldownAmount = length || $el.attr('data-cooldown') || this.length;
+	var cooldownAmount = options.length || $el.attr('data-cooldown') || this.length;
 	if (!cooldownAmount) throw new Error('Error: cooldown: cannot find a cooldown length');
 	
 	// convert to number and handle decimals
 	cooldownAmount += '';
 	cooldownAmount = cooldownAmount.indexOf('.') > -1 ? parseFloat(cooldownAmount) : parseInt(cooldownAmount);
 	
+	// look everywhere for color and opacity, default to #000 0.5 if not found
+	var color = options.color || $el.attr('data-cooldown-color') || this.color || '#000';
+	var opacity = parseFloat(options.opacity || $el.attr('data-cooldown-opacity') || this.opacity || 0.5);
+	
 	// now, to add the cooldown!
 	$el.attr('disabled', 'disabled');
 	$('._tiny_button_cooldown_progress-bar', $el).css({
+		backgroundColor: color,
+		opacity: opacity,
 		height: ($el.height() + this.padding*2) + 'px',
 		width: ($el.width() + this.padding*2) + 'px'
 	}).animate({
@@ -79,9 +92,9 @@ Cooldown.prototype.cooldown = function($el, callback, length) {
 		easing: 'linear',
 		complete: function() {
 			$el.removeAttr('disabled');
-			if (callback) callback($el);
+			if (options.callback) options.callback($el);
 		}
 	});
 	
-	return this; // chain
+	return this;
 };
